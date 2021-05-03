38b4f0a435780b6edc5f3cedcfa3eec93f4bb739
LouisG1977 <luisgonzales272@gmail.com>
2021-05-03 13:47:44 -0600

    davinci: Update Redfin build fingerprint to may
    Signed-off-by: Louis1977 <luisgonzales272@gmail.com>

--- cherish_davinci.mk
+++ cherish_davinci.mk
@@ -45,4 +45,4 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ1A.210205.004/7038034:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys
